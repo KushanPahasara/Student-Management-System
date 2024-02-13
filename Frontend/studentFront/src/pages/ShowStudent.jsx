76a2ca5c04641f34484c34b70db4e91d192ea78f
kushan <kushanpahasaradharmawansha@gmail.com>
2024-02-13 09:55:09 +0530

    Student details

--- Frontend/studentFront/src/pages/ShowStudent.jsx
+++ Frontend/studentFront/src/pages/ShowStudent.jsx
@@ -8,11 +8,51 @@ function ShowStudent() {
   const [book, setBook] = useState({});
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:8070/student/get/${id}`)
+      .then((respose) => {
+        setBook(respose.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+         console.log(error); 
+         setLoading(false);
+      });
+  }, [])
+
   return (
-    <div>
-      
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Books</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Id</span>
+            <span>{book._id}</span>
+
+          </div> 
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Name</span>
+            <span>{book.name}</span>
+
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Age</span>
+            <span>{book.age}</span>
+
+          </div>
+        </div> 
+
+      )}
     </div>
   )
 }
 
 export default ShowStudent
+
+
